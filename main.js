641cd835f0694ebf9aecafa8afc5fe35219a4808
sakthivel-b-20436 <sakthivel.b@zohocorp.com>
2025-12-02 10:07:50 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,22 @@
+async function showProfile() {
+      debugger;
+      await catalyst.auth.isUserAuthenticated().then(result => {
+          document.body.style.visibility = "visible";
+          document.getElementById("fname").innerHTML = "First Name: " + result.content.first_name;
+          document.getElementById("lname").innerHTML = "Last Name: " + result.content.last_name;
+          document.getElementById("mailid").innerHTML = "Email Address: " + result.content.email_id;
+          document.getElementById("tzone").innerHTML = "Time Zone: " + result.content.time_zone;
+          document.getElementById("ctime").innerHTML = "Joined On: " + result.content.created_time;
+      }).catch(err => {
+          document.body.style.visibility = "visible";
+          document.body.innerHTML = 'You are not logged in. Please log in to continue. Redirecting you to the login page..';
+          setTimeout(function () {
+            window.location.href = "/__catalyst/auth/login?redirect_url=/index.html";
+          }, 1500);
+      });
+    }
+
+    function logout() {
+      const redirectURL = location.protocol +"//"+ location.hostname + "/index.html";
+      catalyst.auth.signOut(redirectURL);
+    }
